b9d7f8868c461562095dcf005f7f757f2d9d8238
LuizLimaDev <devluizlima@gmail.com>
2023-11-27 16:50:49 -0300

    refactor: Removing signup/in thirdservice

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -68,66 +68,7 @@ const signInUser = async (req, res) => {
 
 }
 
-const signUpUserThirdService = async (req, res) => {
-  const { name, email, image } = req.body
-
-  if (!name || !email || !image ) {
-    return res.status(400).json({ menssagem: 'Todos os dados são necessários para cadastro.' })
-  }
-
-  try {
-    const response = await knex('users_thirdservice')
-
-    const emailVerification = response.find(user => user.email === email)
-
-    if (emailVerification) {
-      return res.status(400).json({ menssagem: 'Email já existente!' })
-    }
-
-    const signUp = await knex('users_thirdservice')
-      .insert({ name, email, image })
-
-    return res.status(200).json({ menssagem: `Usuário ${name} cadastrado com sucesso!` })
-
-  } catch (error) {
-    return res.status(500).json(error)
-  }
-}
-
-const signInUserThirdService = async (req, res) => {
-  const { name, email } = req.body
-
-  if (!name || !email ) {
-    return res.status(404).json({ menssagem: 'Email ou senha não confere!' })
-  }
-
-  try {
-    const user = await knex('users_thirdservice').where('email', email).first()
-
-    if (!user) {
-      return res.status(404).json({ menssagem: 'Email ou senha não confere!' })
-    }
-
-    const { id, name, email } = user
-    const userLoged = {
-      id,
-      name,
-      email,
-    }
-
-    const token = jwt.sign({ id, name, email, phone }, process.env.JWTPW, { expiresIn: '8h' })
-
-    return res.status(200).json({ userLoged, token })
-
-  } catch (error) {
-    return res.status(500).json(error)
-  }
-
-}
-
 module.exports = {
   signUpUser,
   signInUser,
-  signUpUserThirdService,
-  signInUserThirdService
 }
