9735405e7f4a1e2bb7136fb429c1731bafdd4f42
luizlimadev <devluizlima@gmail.com>
2023-12-11 13:38:19 -0300

    refactor: Removing middleware from products categories route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,53 +1,58 @@
-const express = require('express')
-const routes = express()
-const { signUpUser, signInUser, signUpUserThirdService, signInUserThirdService } = require('../controllers/users')
-const userVerification = require('../middlewares/authentication')
+const express = require("express");
+const routes = express();
+const {
+  signUpUser,
+  signInUser,
+  signUpUserThirdService,
+  signInUserThirdService,
+} = require("../controllers/users");
+const userVerification = require("../middlewares/authentication");
 const {
   createCategories,
   listCategories,
   updateCategories,
-  deleteCategories
-} = require('../controllers/categories')
+  deleteCategories,
+} = require("../controllers/categories");
 const {
   listProducts,
   addProduct,
   updateProducts,
-  deleteProduct
-} = require('../controllers/products')
+  deleteProduct,
+} = require("../controllers/products");
 const {
   userOrder,
   orderItem,
   listOrder,
   listOrderItems,
   deleteUserOrder,
-  deleteItemOrder
-} = require('../controllers/orders')
-const { signUpAdmin, signInAdmin } = require('../controllers/admin')
+  deleteItemOrder,
+} = require("../controllers/orders");
+const { signUpAdmin, signInAdmin } = require("../controllers/admin");
 
-routes.post('/', signInUser)
-routes.post('/signup', signUpUser)
-routes.post('/signinthirdservice', signInUserThirdService)
-routes.post('/signupthirdservice', signUpUserThirdService)
+routes.post("/", signInUser);
+routes.post("/signup", signUpUser);
+routes.post("/signinthirdservice", signInUserThirdService);
+routes.post("/signupthirdservice", signUpUserThirdService);
 
-routes.post('/admin/signup', signUpAdmin)
-routes.post('/admin', signInAdmin)
+routes.post("/admin/signup", signUpAdmin);
+routes.post("/admin", signInAdmin);
 
-routes.post('/categories', userVerification, createCategories)
-routes.get('/categories', userVerification, listCategories)
-routes.put('/categories/:id', userVerification, updateCategories)
-routes.delete('/categories/:id', userVerification, deleteCategories)
+routes.post("/categories", userVerification, createCategories);
+routes.get("/categories", listCategories);
+routes.put("/categories/:id", userVerification, updateCategories);
+routes.delete("/categories/:id", userVerification, deleteCategories);
 
-routes.get('/products', listProducts);
-routes.post('/products', userVerification, addProduct)
-routes.put('/products/:id', userVerification, updateProducts);
-routes.delete('/deleteproducts/:id', userVerification, deleteProduct);
+routes.get("/products", listProducts);
+routes.post("/products", userVerification, addProduct);
+routes.put("/products/:id", userVerification, updateProducts);
+routes.delete("/deleteproducts/:id", userVerification, deleteProduct);
 
-routes.post('/userorder', userVerification, userOrder)
-routes.get('/userorder/:id', userVerification, listOrder)
-routes.delete('/userorder/:id', userVerification, deleteUserOrder)
+routes.post("/userorder", userVerification, userOrder);
+routes.get("/userorder/:id", userVerification, listOrder);
+routes.delete("/userorder/:id", userVerification, deleteUserOrder);
 
-routes.post('/orderitem', userVerification, orderItem)
-routes.get('/orderitems/:id/:orderid', userVerification, listOrderItems)
-routes.delete('/orderitems/:id/:orderid', userVerification, deleteItemOrder)
+routes.post("/orderitem", userVerification, orderItem);
+routes.get("/orderitems/:id/:orderid", userVerification, listOrderItems);
+routes.delete("/orderitems/:id/:orderid", userVerification, deleteItemOrder);
 
 module.exports = routes;
